f142fb6ec13c4f1dbcab42fe55db55e113741192
Kevin Ngo <me@ngokevin.com>
2019-03-11 14:18:38 -0700

    linux toolchain

--- postinstall.js
+++ postinstall.js
@@ -1,22 +1,38 @@
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
+const rimraf = require('rimraf');
 const unzipper = require('unzipper');
 
-const lib = 'toolchain.zip';
+const platform = os.platform();
 
-const rs = fs.createReadStream(path.join(__dirname, lib));
-rs.on('open', () => {
-  const ws = rs.pipe(unzipper.Extract({
-    path: __dirname,
-  }));
-});
+const zips = {
+  darwin: 'toolchain_macos.zip',
+  linux: 'toolchain_linux.zip'
+};
 
-rs.on('error', err => {
-  if (err.code === 'ENOENT') {
-    process.exit(0);
-  } else {
-    throw err;
+Object.keys(zips).forEach(zipPlatform => {
+  if (zipPlatform !== platform) {
+    rimraf(zips[zipPlatform], () => {});
+    return;
   }
+
+  const rs = fs.createReadStream(path.join(__dirname, zips[platform]));
+  rs.on('open', () => {
+    const ws = rs.pipe(unzipper.Extract({
+      path: __dirname,
+    })).on('entry', () => {
+      rimraf(zips[platform], () => {});
+    });
+  });
+
+  rs.on('error', err => {
+    if (err.code === 'ENOENT') {
+      process.exit(0);
+    } else {
+      throw err;
+    }
+  });
 });
 
 process.on('uncaughtException', err => {
